d3baeb0a9a1ce072a2d7a36ba30f309cb2609105
Himanshu <hmsppatel320@gmail.com>
2025-08-01 11:25:42 +0530

    Changes Committed

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ const gridGallery = document.querySelector(".gallery-grid");
 const errorMsg = document.getElementById("errorMsg");
 
 // Hugging face api key
-const API_KEY = "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";
+const API_KEY = "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";
 
 const examplePrompts = [
   "A magic forest with glowing plants and fairy homes among giant mushrooms",
